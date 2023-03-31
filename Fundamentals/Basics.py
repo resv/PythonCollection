6af474783030307969d7addb28ddb14a0c2bd74a
Adam Kim <akim456@gmail.com>
2023-03-31 09:53:05 -0400

    added notes

---
+++ Fundamentals/Basics.py
@@ -0,0 +1,46 @@
+#ONLINE PYTHON COMPILER
+https://www.programiz.com/python-programming/online-compiler/
+
+#PRINT STATEMENT
+print('hello')
+
+#PRINT VARIABLE
+x = 6
+print(x)
+
+#PRINT W/ VARIABLE CONDITION
+x = 43
+x = x + 1
+print(x)
+
+#IF STATEMENT
+#indentation aka 4 spaces is important. Colons are necessary
+#prints smaller then finis, "bigger" is skipped due to the 1st if being statisfied
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+print('Finis')
+
+#WHILE LOOPS
+#condition is true in a loop so it will print until we lower N to 0
+n = 5
+while n > 0 :
+    print (n)
+    n = n - 1
+print('Blastoff!')
+
+#CONSTANTS
+#string constants use single ' or double "
+
+#Reserved words are special words that python are listening for them.
+#Reserved words: False, None, True, and, as, assert, break, class, if, def, del, elif,
+#Reserved words cont: else, except, return, for, from, global, try, import, in, is, lambda
+#Reserved words cont: while, not, or, pass, raise, finally, continue, nonlocal, with, yield
+
+#NAMING CONVENTION
+#Py must start with a letter or underscore_ and case sensitive
+
+#MNEMONIC = MEMORY AID
+#Choosing a variable name that helps us remember what we intend to store.
