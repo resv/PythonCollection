2ac1b605aa9369e1e7974db4710e5e8cb3de218f
Adam Kim <akim456@gmail.com>
2023-10-03 20:50:05 -0400

    added more quick notes

--- Fundamentals/Basics.py
+++ Fundamentals/Basics.py
@@ -51,3 +51,24 @@ print('Blastoff!')
 #Logical operators: and, or, not
 #Loops: for, in, while, break, continue
 #Functions: def, return  
+
+#Arithmetic operators
+# x + y            Addition + operator returns the sum of x plus y
+# x - y             Subtraction - operator returns the difference of x minus y
+# x * y            Multiplication * operator returns the product of x times y
+# x / y             Division / operator returns the quotient of x divided by y
+# x**e            Exponent ** operator returns the result of raising x to the power of e 
+# x**2            Square expression returns x squared
+# x**3            Cube expression returns x cubed
+# x**(1/2)    Square root (½) or (0.5) fractional exponent operator returns the square root of x
+# x // y           Floor division operator returns the integer part of the integer division of x by y
+# x % y          Modulo operator returns the remainder part of the integer division of x by y
+
+
+#Order of operations
+#The order of operations are to be calculated from left to right in the following order
+#Parentheses ( ), { }, [ ]
+#Exponents xe   (x**e)
+#Multiplication * and Division /  
+#Addition + and Subtraction -    
+#You might find the PEMDAS mnemonic device to be helpful in remembering the order.   
