cb9bef31e181d40febb7f05e2654768b7152a322
Adam Kim <akim456@gmail.com>
2023-10-17 21:31:13 -0400

    Update Basics.py

--- Fundamentals/Basics.py
+++ Fundamentals/Basics.py
@@ -436,15 +436,52 @@ string.split(delimiter) - Returns a list of substrings that were separated by wh
 string.replace(old, new) - Returns a new string where all occurrences of old have been replaced by new.
 delimiter.join(list of strings) - Returns a new string with all the strings joined by the delimiter 
 ----------------------------------------------------------------------------------------------------------------------------------
-"strings {}".format(variable) <- variable goes into the empty {}
+#FORMAT
+# "strings {}".format(variable) <- variable goes into the empty {}
 
 name = "my name is {}, hi".format(Jason)
 print(name)
 """Outputs:
 my name is Jason, hi
 """
-----------------------------------------------------------------------------------------------------------------------------------
+--------------------------
+#If the placeholders indicate a number, they’re replaced by the variable corresponding to that order (starting at zero).
+first = "apple"
+second = "banana"
+third = "carrot"
 
+formatted_string = "{0} {2} {1}".format(first, second, third)
+
+print(formatted_string)
+
+"""Outputs:
+apple carrot banana
+"""
+--------------------------
+"{var1} {var2}".format(var1=value1, var2=value2)
+--------------------------
+# {:d} integer value
+'{:d}'.format(10.5) → '10'
+
+
+#Formatting expressions
+Expr       Meaning                                       Example
+{:d}       integer value                                 '{:d}'.format(10.5) → '10'
+{:.2f}     floating point with that many decimals        '{:.2f}'.format(0.5) → '0.50'
+{:.2s}     string with that many characters              '{:.2s}'.format('Python') → 'Py'
+{:<6s}     string aligned to the left that many spaces   '{:<6s}'.format('Py') → 'Py'
+{:>6s}     string aligned to the right that many spaces  '{:>6s}'.format('Py') → '    Py'
+{:^6s}     string centered in that many spaces           '{:^6s}'.format('Py') → '  Py  '
+----------------------------------------------------------------------------------------------------------------------------------
+#String Operations and Methods
+.format() - String method that can be used to concatenate and format strings. 
+{:.2f} - Within the .format() method, limits a floating point variable to 2 decimal places. The number of decimal places can be customized.
+len(string) - String operation that returns the length of the string.
+string[x] - String operation that accesses the character at index [x] of the string, where indexing starts at zero.
+string[x:y] - String operation that accesses a substring starting at index [x] and ending at index [y-1]. If x is omitted, its value defaults to 0. 
+            If y is omitted, the value will default to len(string).
+string.replace(old, new) - String method that returns a new string where all occurrences of an old substring have been replaced by a new substring.
+string.lower() - String method that returns a copy of the string with all lowercase characters.
 
 
 ----------------------------------------------------------------------------------------------------------------------------------
