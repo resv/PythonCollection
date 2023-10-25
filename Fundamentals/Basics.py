9a355f18635dab59576bc4744287b42eca093d10
Adam Kim <akim456@gmail.com>
2023-10-25 02:07:09 -0400

    Update Basics.py

--- Fundamentals/Basics.py
+++ Fundamentals/Basics.py
@@ -501,8 +501,39 @@ Here is your output:
 ['Orange', 'Strawberry', 'Peach']
 
 ----------------------------------------------------------------------------------------------------------------------------------
+List comprehension
+def squares(start, end):
+    return [n**2 for n in range(start,end+1) ]
 
 
+print(squares(2, 3))  # Should print [4, 9]
+print(squares(1, 5))  # Should print [1, 4, 9, 16, 25]
+print(squares(0, 10)) # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+-----
+### List Comprehension with Conditional Statement
+print("List comprehension result:")
+
+# The following list comprehension compacts multiple lines 
+# of code into one line:
+print([ x for x in range(1,101) if x % 10 == 0 ])
+
+### Long form for loop with nested if-statement
+print("Long form code result:")
+
+# The list comprehension above accomplishes the same result as
+# the long form version of the code:
+my_list = []
+for x in range(1,101):
+  if x % 10 == 0:
+    my_list.append(x)
+print(my_list)
+
+results:
+List comprehension result:
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+Long form code result:
+[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 ----------------------------------------------------------------------------------------------------------------------------------
 
