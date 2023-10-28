a50fdd026eb4e099ec2fcdeec7bbcc9652a8b164
Adam Kim <akim456@gmail.com>
2023-10-28 16:35:58 -0400

    Update Basics.py

--- Fundamentals/Basics.py
+++ Fundamentals/Basics.py
@@ -591,8 +591,26 @@ Long form code result:
 [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
 ----------------------------------------------------------------------------------------------------------------------------------
+## DICTIONARY KEYVALUE PAIRS
+e.g.
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for animal, feature in cool_beasts.items():
+    print("{} have {}".format(animal, feature))
 
+result:
+Here is your output:
+octopuses have tentacles
+dolphins have fins
+rhinos have horns
 
+########### Iterating Over Dictionaries
+You can iterate over dictionaries using a for loop, just like with strings, lists, and tuples. This will iterate over the sequence of 
+keys in the dictionary. If you want to access the corresponding values associated with the keys, you could use the keys as indexes. 
+Or you can use the items method on the dictionary, like dictionary.items(). This method returns a tuple for each element in the dictionary, 
+where the first element in the tuple is the key and the second is the value.
+
+If you only wanted to access the keys in a dictionary, you could use the keys() method on the dictionary: dictionary.keys(). If you 
+only wanted the values, you could use the values() method: dictionary.values().
 ----------------------------------------------------------------------------------------------------------------------------------
 
 
