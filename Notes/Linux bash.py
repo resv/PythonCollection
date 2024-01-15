6cfe0d4a38d012ee6585277bea8180ff7319652d
Adam Kim <akim456@gmail.com>
2024-01-15 16:04:47 -0500

    Create Linux bash.py

---
+++ Notes/Linux bash.py
@@ -0,0 +1,10 @@
+----- MAKE DIR | mkdir <name> ---------------------------------------------------------------
+----- REMOVE DIR | rmdir <name>
+----- COPY copy this file at this directory to new directory | cp <path> <newpath> ---------------------------------------------------------------
+cp ../filename.txt .  | .. = (old) parent directory  . = (new) current directory
+----- COPY SIMPLE #1 | cp <oldname> <newname> ----
+----- RENAME or MOVE | mv <oldname> <newname> ----
+----- 
+----- NEW FILE | touch filename.txt---------------------------------------------------------------
+----- REMOVE | rm <filename>  ---------------------------------------------------------------
+----- REMOVE ALL | rm *  ---------------------------------------------------------------
