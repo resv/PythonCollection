609334cafa366d36af7c748af8a491e5be2dc637
Adam Kim <akim456@gmail.com>
2024-01-26 19:15:09 -0500

    Update Basics.py

--- Notes/Basics.py
+++ Notes/Basics.py
@@ -1,3 +1,32 @@
+################## automation formula ##########################################
+Is automation worthwhile?
+To determine if automating a process would save labor time, use the following formula:
+
+Time_to_automate < (time_to_perform * amount_of_times_done)
+
+e.g.: A banking company is looking to automate one of its internal processes that takes 
+about 40 minutes each week to complete. The automation process will take 10 hours total 
+to complete. How many weeks will it be before the banking company begins to save time 
+on the process and automation would be worthwhile?
+
+time_to_automate = 10 hours
+time_to_perform = 40 minutes
+Amount_of_times_done = x 
+
+Pro tip: Notice that you have two measurements of time. Before plugging values into the formula, convert 10 hours to minutes.
+Multiply 10 by 60 to convert 10 hours into minutes. You get 600 minutes. 
+When you plug your values into the formula from above, you get:
+
+600 < 40x
+
+Pro tip: x represents the number of weeks, so you can replace that with x in the formula.
+Then, divide both sides by 40 to get:
+	15 < weeks
+It will take 15 weeks before the banking company starts saving time on the process. 
+Remember, automating this process only needs to occur once. In addition, it helps 
+employees by removing the need to think about the task and keeping up with the tracking of it.
+
+###################################################################################################
 #ONLINE PYTHON COMPILER
 https://www.programiz.com/python-programming/online-compiler/
 
