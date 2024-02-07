94463b7cce1edf2754d6507911ebc250e22b7fc2
Adam Kim <akim456@gmail.com>
2024-02-06 19:29:51 -0500

    addd notes

--- Notes/Using Git.py
+++ Notes/Using Git.py
@@ -37,7 +37,53 @@ git commit (opens text editor)
 git commit -m '<MESSAGE>'
 
 -----   ---------------------------------------------------------------
+#Study guide: Git
+#There are three sections: 
+1. Git directory, 
+2. Working tree, 
+3. Staging area.
 
------   ---------------------------------------------------------------
+#Git config command
+#The Git config command is used to set the values to identify who made changes to Git repositories. 
+#To set the values of user.email and user.name to your email and name, type: 
+#: ~$ git config  - -global user.email "#me@example.com"
+#: ~$ git config  - -global user.name “My name”
 
+#Git init command = can create a new empty repository in a current directory or re-initialize an existing one. 
+#: ~/checks$ git init
+
+#Git ls -la command = checks that an identified directory exists.
+#: ~/checks$ ls -la
+
+#The ls-l.git checks inside the directory to see the different things that it contains. 
+#This is called the Git directory. The Git directory is a database for your Git project 
+#that stores the changes and the change history.
+#: ~/checks$ ls -l .git/
+
+#Git add command allows Git to track your file and uses the selected file as a parameter 
+#when adding it to the staging area. The staging area is a file maintained by Git that 
+#contains all the information about what files and changes are going to go into your next commit.
+#:~/checks$ git add disk_usage.py
+
+#Git status command = to get some information about the current working tree and pending changes.
+#:~/checks$ git status
+
+#Git commit command = to remove changes made from the staging area to the .git directory. When 
+#this command is run, it tells Git to save changes. A text editor is opened that allows a commit message to be entered.
+#:~/checks$ git commit
+
+
+#Guidelines for writing commit messages
+#A commit message is generally broken into two sections: a short summary and a description of the changes. 
+#When the git commit command is run, Git will open a text editor to write your commit message. A good commit 
+#message includes the following:
+
+#Summary: The first line contains the summary, formatted as a header, containing 50 characters or less. 
+
+#Description: The description is usually kept under 72 characters and provides detailed information about 
+#the change. It can include references to bugs or issues that will be fixed with the change. It also can 
+#include links to more information when relevant. 
+
+#Example of a commit message:  #https://commit.style/
+ 
 -----   ---------------------------------------------------------------
