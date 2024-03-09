f9cd590a635d6a2f5795aaf964ab02c9ce1473ce
Adam Kim <akim456@gmail.com>
2024-03-09 15:39:21 -0500

    Update Using Git.py

--- Notes/Using Git.py
+++ Notes/Using Git.py
@@ -236,3 +236,29 @@ git fetch
 
 # lists remote branches and can be combined with other branch arguments to manage remote branches.
 git branch -r
+
+
+--------------------------------------- NOTES ---------------------------------------
+# CI/CD: The name for the entire continuous integration and continuous deployment system
+
+# Code reviews: The deliberate and methodical gathering of other programmers to examine each other's code for errors to increase the code quality and reduces the amount of bugs
+
+# Continuous deployment (CD): New code is deployed often after it has been automatically built and tested
+
+# Continuous integration (CI): A system that will automatically build and test our code every time there's a change
+
+# Fix up: The decision to discard commit messages for that commit 
+
+# Forking: A way of creating a copy of the given repository so that it belongs to our user
+
+# Indirect merges: GitHub can merge a pull request automatically if the head branch is directly or indirectly merged into the base branch externally
+
+# Issue tracker (bug tracker): A tracker that shows tasks that need to be done, the state they're in and who's working on them
+
+# Merge commits: All commits from the feature branch are added to the base branch
+
+# Pipelines: The specific steps that need to run to obtain the desired result
+
+# Pull request: A procedure where new code is examined before it is merged to create a branch or master branch 
+
+# Squash commits: The decision add commit messages together and an editor opens to make any necessary changes
