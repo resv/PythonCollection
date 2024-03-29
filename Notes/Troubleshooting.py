b555be6842a17c1f823621038bb89b44ac925730
Adam Kim <akim456@gmail.com>
2024-03-29 11:49:26 -0400

    Create Troubleshooting.py

---
+++ Notes/Troubleshooting.py
@@ -0,0 +1,42 @@
+----- ---------------------------------------------------------------
+
+
+-----Windows processes ---------------------------------------------------------------
+Windows Process Monitor, also known as Sysinternals
+Windows Process Explore -  file monitoring and analyzing processes on Windows computers.
+-----Linux performance ---------------------------------------------------------------
+Perf-tools, bcc/BPF, and bpftrace.
+
+-----  USE method---------------------------------------------------------------
+USE Method is essential for optimizing system performance and troubleshooting server
+
+----- ---------------------------------------------------------------
+Caching
+Although a cache is not a monitoring tool, it's important not to overlook them as computing relies heavily on caches, which enhance data access speed and overall system performance
+
+----- ---------------------------------------------------------------
+
+Linux autogrouping
+In Linux, autogrouping optimizes desktop performance during CPU-intensive workloads by grouping processes and ensuring fair CPU cycle distribution. 
+Autogrouping tells the process scheduler component in Linux to act based on a group’s configured “nice level” instead of individual processes. 
+However, autogrouping can interfere with traditional processes. When enabled, the ”nice” value primarily affects priority within the group, 
+reducing the effectiveness of “nice” and ”renice” commands. Even programs setting their own nice levels may still receive a "fair" share of CPU time.
+
+
+----- ---------------------------------------------------------------
+
+
+----- ---------------------------------------------------------------
+
+
+----- ---------------------------------------------------------------
+----- -------------------------------------------------------------------- ---------------------------------------------------------------
+
+----- ---------------------------------------------------------------
+----- ---------------------------------------------------------------
+----- ---------------------------------------------------------------
+----- ---------------------------------------------------------------
+----- ---------------------------------------------------------------
+----- ---------------------------------------------------------------
+----- ---------------------------------------------------------------
+----- -------------------------------------------------------------------- ---------------------------------------------------------------
