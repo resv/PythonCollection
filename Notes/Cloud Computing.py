eff87017f7a4e755ccfe4ec9876cc4e9d44cc7e5
Adam Kim <akim456@gmail.com>
2024-04-29 20:20:36 -0400

    Create Cloud Computing.py

---
+++ Notes/Cloud Computing.py
@@ -0,0 +1,58 @@
+Terms and definitions from Course 5, Module 1
+A/B testing: A way to compare two versions of something to find out which version performs better
+
+Automatic scaling: This service uses metrics to automatically increase or decrease the capacity of the system
+
+Autoscaling: Allows the service to increase or reduce capacity as needed, while the service owner only pays for the cost of the machines that are in use at any given time
+
+Capacity: How much the service can deliver
+
+Cold data: Accessed infrequently and stored in cold storage
+
+Containers: Applications that are packaged together with their configuration and dependencies
+
+Content Delivery Networks (CDN): A network of physical hosts that are geographically located as close to the end users as possible
+
+Disk image: A snapshot of a virtual machine’s disk at a given point in time
+
+Ephemeral storage: Storage used for instances that are temporary and only need to keep local data while they’re running
+
+Hot data: Accessed frequently and stored in hot storage
+
+Hybrid cloud: A mixture of both public and private clouds
+
+Input/Output Operations Per Second (IOPS): Measures how many reads or writes you can do in one second, no matter how much data you're accessing
+
+Infrastructure as a Service (or IaaS): When a Cloud provider supplies only the bare-bones computing experience
+
+Load balancer: Ensures that each node receives a balanced number of requests
+
+Manual scaling: Changes are controlled by humans instead of software
+
+Multi-cloud: A mixture of public and/or private clouds across vendors
+
+Object storage: Storage where objects are placed and retrieved into a storage bucket
+
+Orchestration: The automated configuration and coordination of complex IT systems and services
+
+Persistent storage: Storage used for instances that are long lived and need to keep data across reboots and upgrades
+
+Platform as a Service (or PaaS): When a Cloud provider offers a preconfigured platform to the customer
+
+Private cloud: When your company owns the services and the rest of your infrastructure
+
+Public cloud: The cloud services provided to you by a third party
+
+Rate limits: Prevent one service from overloading the whole system
+
+Reference images: Store the contents of a machine in a reusable format
+
+Software as a Service (or SaaS): When a Cloud provider delivers an entire application or program to the customer
+
+Sticky sessions: All requests from the same client always go to the same backend server
+
+Templating: The process of capturing all of the system configuration to let us create VMs in a repeatable way
+
+Throughput: The amount of data that you can read and write in a given amount of time
+
+Utilization limits: Cap the total amount of a certain resource that you can provision
